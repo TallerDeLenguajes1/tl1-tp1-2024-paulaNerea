470bcc5a93f61844a35fc82e2fb65e52ecbe68a2
paulaNerea <panelevi@gmail.com>
2024-04-06 14:04:40 -0300

    segundo commit

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    printf("hola mundo");
+    int variable = 15, *puntero;
+    puntero = &variable;
+
+    printf("hola mundo\n");
+
+    printf("Contenido del puntero: %d\n", *puntero);
+    printf("Direccion de memoria almacenada por el puntero: %p\n", puntero);
+    printf("Direccion de memoria de la variable: %p\n", &variable);
+    printf("Direccion de memoria del puntero: %p\n", &puntero);
+    printf("Tamanio de memoria utilizado por esa variable: %zu bytes\n", sizeof(variable));
+
     return 0;
 }
